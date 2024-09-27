fd92dfe370387ce16effb9c20b7f499a0079790d
João Maduro <joao.maduro@sydle.com>
2024-09-27 17:49:31 -0300

    fix: audiochart scroll

--- components/AudioChart/AudioChart.tsx
+++ components/AudioChart/AudioChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { LineChart } from 'react-native-chart-kit';
 import { Dimensions, View, ScrollView } from 'react-native';
 
@@ -8,16 +8,25 @@ interface AudioChartProps {
 
 const AudioChart: React.FC<AudioChartProps> = ({ data }) => {
   const screenWidth = Dimensions.get('window').width;
+  const scrollViewRef = useRef<ScrollView>(null);
 
   let modifiedData = data.length > 2 ? data.slice(1, -1) : data;
-  modifiedData = modifiedData.map((value) => value + 0);
+
+  useEffect(() => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollToEnd({ animated: false });
+    }
+  }, [data]); 
 
   return (
     <View style={{ backgroundColor: 'transparent', borderRadius: 16 }}>
-      <ScrollView horizontal>
+      <ScrollView
+        horizontal
+        ref={scrollViewRef} // Atribui a referência ao ScrollView
+      >
         <LineChart
           data={{
-            labels: [], 
+            labels: [],
             datasets: [
               {
                 data: modifiedData,
@@ -25,10 +34,11 @@ const AudioChart: React.FC<AudioChartProps> = ({ data }) => {
               },
             ],
           }}
-          width={Math.max(screenWidth, modifiedData.length * 4)}
+          width={Math.max(screenWidth, modifiedData.length * 6)}
           height={320}
           yAxisLabel=""
           yAxisSuffix=""
+          withShadow={false}
           yAxisInterval={1}
           chartConfig={{
             backgroundGradientFrom: "transparent",
@@ -36,8 +46,8 @@ const AudioChart: React.FC<AudioChartProps> = ({ data }) => {
             backgroundGradientTo: "transparent",
             backgroundGradientToOpacity: 0,
             decimalPlaces: 4,
-            color: (opacity = 1) => `rgba(255, 69, 0, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(0, 0, 0, 1)`,
+            color: (opacity = 1) => `transparent`,
+            labelColor: (opacity = 1) => `#fff`,
             style: {
               borderRadius: 16,
             },
